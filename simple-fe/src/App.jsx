53235ea126a87573ba8ca186b56e61da0e453662
Olawale O Samson <smsnmicheal@gmail.com>
2022-04-25 21:04:16 +0100

    done; awating review

--- simple-fe/src/App.jsx
+++ simple-fe/src/App.jsx
@@ -1,44 +1,38 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import React, { useState } from 'react'
+import { Button, Stack, ThemeProvider, Typography } from '@mui/material'
 
-function App() {
-  const [count, setCount] = useState(0)
+import { Navbar, Textbox } from './components'
+import { theme } from './theme'
+
+const App = () => {
+  const [isShown, setIsShown] = useState(false)
+
+  const toggle = () => setIsShown(!isShown)
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Navbar />
+      <main>
+        <Stack>
+          <Typography variant='h4'>
+            Welcome to the API hub,
+          </Typography>
+          <Typography variant='subtitle1'>
+            Here you will find our collection of APIs for developers.
+          </Typography>
+        </Stack>
+        <Button variant='contained' onClick={toggle}>
+          Enter API hub
+        </Button>
+
+        {isShown &&
+        <Stack direction={{ xs: 'column', md: 'row'}} spacing={3}>
+          <Textbox />
+          <Textbox />
+          <Textbox />
+        </Stack>}
+      </main>
+    </ThemeProvider>
   )
 }
 
