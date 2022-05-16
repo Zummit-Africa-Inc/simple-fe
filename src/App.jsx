1019df9dbddfffa34782ce474ec4b4f9da1d3d89
Olawale O Samson <smsnmicheal@gmail.com>
2022-05-16 12:47:18 +0100

    changed navbar position so it is always visible on any page

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,17 @@ import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 
 import { Home } from './pages'
+import { Navbar } from './components'
 
 const App = () => {
   
-
   return (
+    <>
+    <Navbar />
     <Routes>
       <Route path='/' element={<Home />} />
     </Routes>
+    </>
   )
 }
 
